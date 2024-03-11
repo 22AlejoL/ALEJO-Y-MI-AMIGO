d1e446abc76c0d53273b9e4f159ce937627214f3
22AlejoL <143149386+22AlejoL@users.noreply.github.com>
2024-03-11 10:50:48 -0500

    Update Colas.cpp

--- Colas.cpp
+++ Colas.cpp
@@ -4,16 +4,16 @@
 using namespace std;
 template <typename T>
 
-class Pila
+class Cola
 {
     private: 
         vector<T> storage;
 
     public:
-        Pila() : storage() {}
+        Cola() : storage() {}
 
     /*  
-    EVALUAR SI LA PILA ESTÁ VACÍA
+    EVALUAR SI LA Cola ESTÁ VACÍA
 
     COMPLEJIDAD : O(1) porque storage empty es O(1).
     */
@@ -23,23 +23,19 @@ class Pila
         }
 
     /*
-    Retorna el número de elementos en la pila
+    Retorna el número de elementos en la Cola
     */
 
         unsigned int size() const { return storage.size(); }
     
     /*
-    Inserta el elemento en la pila.
+    Inserta el elemento en la Cola.
     Complejidad: constante amortoizada.
     */
 
         void push(const T &elem) { storage.push_back(elem); }
-        void pop() { storage.pop_back(); }
-        const T &top() const { return storage.back(); }
-        T &top() { return storage.back(); }
+        void pop() { storage.pop_front(); }
         void print() const {
-            for (const T &1 : storage) {
-                cout << " " << i; 
-            }
+             storage.print();
         }
 };
