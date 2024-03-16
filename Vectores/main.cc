e47bb74c72798ab93101fde114c4550b39a3010c
Berny02 <143149231+Berny02@users.noreply.github.com>
2024-03-16 13:42:59 -0500

    Update main.cc

--- Vectores/main.cc
+++ Vectores/main.cc
@@ -1,24 +1,19 @@
-
-int main() {
-    Vector v;
-    v.push_back(1);
-    v.push_back(2);
-    v.push_back(3);
-    v.push_back(4);
-    v.push_back(5);
-    v.push_back(6);
-    v.push_back(7);
-    v.push_back(8);
-    v.push_back(9);
-    v.push_back(10);
-    cout << "Front: " << v.front() << endl; // Imprime 1
-    cout << "Back: " << v.back() << endl;   // Imprime 3
-    cout << "Elemento en la posición 1: " << v.at(1) << endl; // Imprime 2
-    v.pop_back();
-    cout << "Back después de pop_back(): " << v.back() << endl; // Imprime 2
-    v.pop_front();
-    cout << "Front después de pop_front(): " << v.front() << endl; // Imprime 2
-    v.push_front(5);
-    cout << "Front después de push_front(5): " << v.front() << endl; // Imprime 5
+int main(){
+  //Create a vector with {1,2,3,4}
+  Vector v(4, 0);
+  //initialize
+  v.initialize();
+  //print
+  v.print();
+  //push_back
+  v.push_back(5);
+  //print
+  v.print();
+  //empty?
+  cout << v.empty() << endl;
+  //push_front
+  v.push_front(3);
+  //print
+  v.print();
     return 0;
 }
