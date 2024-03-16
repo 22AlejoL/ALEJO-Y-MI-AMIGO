ee674f38a28ae84481ef162da18e7a27197b7ae5
22AlejoL <143149386+22AlejoL@users.noreply.github.com>
2024-03-16 13:13:00 -0500

    Update Colas.hh

--- Queue/Colas.hh
+++ Queue/Colas.hh
@@ -1,1 +1,42 @@
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+template <typename T>
+
+class Cola
+{
+    private: 
+        vector<T> storage;
+
+    public:
+        Cola() : storage() {}
+
+    /*  
+    EVALUAR SI LA Cola ESTÁ VACÍA
+
+    COMPLEJIDAD : O(1) porque storage empty es O(1).
+    */
+
+        bool empty() const {
+            return storage.empty();
+        }
+
+    /*
+    Retorna el número de elementos en la Cola
+    */
+
+        unsigned int size() const { return storage.size(); }
+    
+    /*
+    Inserta el elemento en la Cola.
+    Complejidad: constante amortoizada.
+    */
+
+        void push(const T &elem) { storage.push_back(elem); }
+        void pop() { storage.pop_front(); }
+        void print() const {
+             storage.print();
+        }
+};
