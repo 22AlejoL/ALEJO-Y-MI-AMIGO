e160f216ef3c914847bd137de6b2b41ed41db7a1
22AlejoL <143149386+22AlejoL@users.noreply.github.com>
2024-03-16 15:08:19 -0500

    Update main.cc

--- Miamigogauss/main.cc
+++ Miamigogauss/main.cc
@@ -1,1 +1,34 @@
+#include <iostream>
+#include "vector.hh"
+#include "Gauss.hh"
 
+#define CYAN    "\x1b[36m"
+#define RESET   "\033[0m"
+#define ROJO    "\033[31m"     
+#define VERDE   "\033[32m"      
+#define AZUL    "\033[34m"      
+#define YELLOW  "\x1b[33m"
+#define MAGENTA "\x1b[35m"
+
+int main() {
+    int filas, columnas;
+    cout << CYAN "Ingrese el número de filas de la matriz: " RESET;
+    cin >> filas;
+    cout << CYAN "Ingrese el número de columnas de la matriz: " RESET;
+    cin >> columnas;
+
+    Matriz m(filas, columnas);
+    m.inicializar();
+
+    cout << VERDE "La matriz ingresada es:" RESET << endl;
+    m.imprimir();
+    cout << endl;
+
+    // resolver la matriz
+    m.gaussJordan();
+
+    cout << VERDE "La matriz después de la eliminación Gauss-Jordan es:" RESET << endl;
+    m.imprimir();
+
+    return 0;
+}
