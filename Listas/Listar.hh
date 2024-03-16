1fcf3e49cc0b9605b37f422a3b5ef23773044286
Berny02 <143149231+Berny02@users.noreply.github.com>
2024-03-16 13:11:42 -0500

    Update Listar.hh

--- Listas/Listar.hh
+++ Listas/Listar.hh
@@ -1,1 +1,56 @@
+#include <ioestream>
+using namespace std;
 
+template<typename T>
+class List{
+private:
+class Node {
+Node* next_;
+T data;
+public:
+Node(): next_(nullptr), data(T()) {}
+Node (const T& d) next_(nullptr), data_(d) {}
+void setNext (Node* p){
+  next_ = p;
+}
+Node* getNext () { return next_; }
+};
+
+// Atributos
+Node* first_;
+Node* last_;
+unsigned int size_;
+public:
+list() : first_(nullptr), last_(nullptr), size_(0) {}
+insigned int size_() const {return size_;}
+bool empty () {
+  if (first_ == nulptr){
+    true;
+}}
+void push_back (T& elem) {
+  Node* n = new Node(elem);
+  if (empty()) {
+    first_ = n;
+  }else{
+    last_->setNext(n);
+  }
+  last_ = n;
+  size++;
+}
+
+void push_front (T& elem) {
+  Node* n = new Node(elem);
+  if (empty()) {
+    first_ = n;
+    last_ =n;
+    size++;
+  }else {
+  //El siguiente de n debe apuntar a lo que apunta firts
+  n->setNext(first_);
+    //fisrt deberia apuntar a n
+    first_ = n;
+  //incrementar size
+    size++;
+  }
+}
+}
