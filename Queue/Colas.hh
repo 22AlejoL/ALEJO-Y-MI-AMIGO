ef1eb64afe274765ce071a9c504adfcef93648b4
Berny02 <143149231+Berny02@users.noreply.github.com>
2024-03-16 13:39:02 -0500

    Update Colas.hh

--- Queue/Colas.hh
+++ Queue/Colas.hh
@@ -1,42 +1,66 @@
+#ifndef _QUEUE_HH_
+#define _QUEUE_HH_
 
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<vector>
 
-using namespace std;
-template <typename T>
+template <class T> class queue {
+  private:
+  int front, rear, size;
+  unsigned cap;
+  T* arr;
+  public:
+  /**
+   * @brief Queue maker
+   *
+   * @return queue
+   */
+  queue(unsigned cap){
+    this->cap = cap;
+    front = size = 0;
+    rear = cap - 1;
+    arr = new T[(cap * sizeof(T))];
+  };
 
-class Cola
-{
-    private: 
-        vector<T> storage;
+  /**
+   * @brief Push a new element in the queue
+   *
+   * @return T item
+   */
+  queue* push(T item){
+    if(size == cap){
+      return this;
+    }
+    rear = (rear + 1) % cap;
+    arr[rear] = item;
+    size = size + 1;
+    return this;
+  };
 
-    public:
-        Cola() : storage() {}
+  /**
+   * @brief Pop the last element in the queue
+   *
+   */
+  void pop(){
+    if(size == 0){
+      return;
+    }
+    front = (front + 1) % cap;
+    size = size - 1;
+  };
 
-    /*  
-    EVALUAR SI LA Cola ESTÁ VACÍA
+  /**
+   * @brief Print the elements in the queue
+   *
+   */
+  void printQueue(){
+    for(int i = 0; i < size; i++){
+      std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+  };
 
-    COMPLEJIDAD : O(1) porque storage empty es O(1).
-    */
 
-        bool empty() const {
-            return storage.empty();
-        }
-
-    /*
-    Retorna el número de elementos en la Cola
-    */
-
-        unsigned int size() const { return storage.size(); }
-    
-    /*
-    Inserta el elemento en la Cola.
-    Complejidad: constante amortoizada.
-    */
-
-        void push(const T &elem) { storage.push_back(elem); }
-        void pop() { storage.pop_front(); }
-        void print() const {
-             storage.print();
-        }
 };
+
+#endif
