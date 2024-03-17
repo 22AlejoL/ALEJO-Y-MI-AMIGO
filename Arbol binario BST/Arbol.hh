edfe078bea8ccd77fc3bc2816d80ad632bec6b34
Berny02 <143149231+Berny02@users.noreply.github.com>
2024-03-17 12:23:17 -0500

    Update Arbol.hh

--- Arbol binario BST/Arbol.hh
+++ Arbol binario BST/Arbol.hh
@@ -3,8 +3,31 @@
 
 #include <ioestream>
 
+//Insertar nodo
+//Mostrar arbol
+//Buscar nodo
+//Recorrer arbol
+//Borrar nodo
 template<typename T>
 class BST {
+    private:
+    T data;
+    Node *izq;
+    Node *der;
+    
+    public:
+    BST (): izq(nullptr), der(nullptr), data(0);
 
+    BST (T elem): data = elem, izq = nullptr, der = nullptr;
+
+    bool empty(){
+        return !root = nullptr;
+    }
+
+    void Insert (T elem){
+        *BST n = new BST(elem);
+
+    }
+    
 }
 #endif
