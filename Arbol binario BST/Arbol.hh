1b5e5fe5c8c8c4a399246a60237f6ab491bf9909
Berny02 <143149231+Berny02@users.noreply.github.com>
2024-03-18 10:21:31 -0500

    Update Arbol.cpp

--- Arbol binario BST/Arbol.hh
+++ Arbol binario BST/Arbol.hh
@@ -1,7 +1,7 @@
-#ifndef _QUEUE_HH_
-#define _QUEUE_HH_
 
-#include <ioestream>
+
+#include <iostream>
+
 
 //Insertar nodo
 //Mostrar arbol
@@ -11,9 +11,36 @@
 template<typename T>
 class BST {
     private:
-    T data;
-    Node *izq;
-    Node *der;
+     class Node {
+    private:
+        Node* izq; // Puntero a izquierda
+        Node* der; // Puntero a derecha
+        T data_; // Datos del nodo
+
+    public:
+        // Constructor por defecto
+        Node() : izq(nullptr), der(nullptr), data_(T()) {}
+
+        // Constructor con inicialización de datos
+        Node(const T& d) : izq(nullptr),der(nullptr), data_(d) {}
+
+        // Establecer el siguiente nodo en la lista
+        void setNext(Node* p) { next_ = p; }
+        
+
+        // Obtener el siguiente nodo en la lista
+        Node* getNext() { return next_; }
+
+        // Obtener los datos del nodo
+        const T& getData() const {
+            return data_;
+        }
+        T& getData() {
+            return data_;
+        }
+        Node* Raiz;
+        unsigned int size = 0;
+    };
     
     public:
     BST (): izq(nullptr), der(nullptr), data(0);
@@ -21,13 +48,40 @@ class BST {
     BST (T elem): data = elem, izq = nullptr, der = nullptr;
 
     bool empty(){
-        return !root = nullptr;
+        BST* n;
+        return n = nullptr;
     }
 
     void Insert (T elem){
-        *BST n = new BST(elem);
-
+        if(empty()){
+            *BST n = New BST(elem);
+        }
+        else{
+            if(elem > n->data){
+                n->der = Insert(n->der,elem);
+            }
+            else if{
+                n->izq = Insert(n->izq,elem);
+            }
+        }
+        return n;
+    }
+    void Ordenar(BST* n){
+        if (empty()){
+            return 0;
+        }
+        Ordenar(n->izq);
+        cout << n->data << " ";
+        Ordenar(n->der);
     }
     
+};
+
+int main(){
+    BST<int> a;
+    a.Insert(32);
+    a.Insert(12);
+    a.Ordenar(a);
 }
-#endif
+
+
