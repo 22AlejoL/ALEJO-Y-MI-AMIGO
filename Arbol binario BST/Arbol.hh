0902b7b3fabf4d5e99aa60ff54d46edca6ce1545
Berny02 <143149231+Berny02@users.noreply.github.com>
2024-03-22 15:19:45 -0500

    Codigo con todos los metodos

--- Arbol binario BST/Arbol.hh
+++ Arbol binario BST/Arbol.hh
@@ -1,87 +1,178 @@
-
+#ifndef ARBOL_BINARIO
+#define ARBOL_BINARIO
 
 #include <iostream>
+using namespace std;
 
-
-//Insertar nodo
-//Mostrar arbol
-//Buscar nodo
-//Recorrer arbol
-//Borrar nodo
 template<typename T>
-class BST {
+class binary_tree {
+private:
+    class Node {
     private:
-     class Node {
-    private:
-        Node* izq; // Puntero a izquierda
-        Node* der; // Puntero a derecha
-        T data_; // Datos del nodo
-
+        T data;
+        Node* right;
+        Node* left;
     public:
-        // Constructor por defecto
-        Node() : izq(nullptr), der(nullptr), data_(T()) {}
-
-        // Constructor con inicialización de datos
-        Node(const T& d) : izq(nullptr),der(nullptr), data_(d) {}
-
-        // Establecer el siguiente nodo en la lista
-        void setNext(Node* p) { next_ = p; }
+        Node() : data(), right(nullptr), left(nullptr) {}
+        Node(const T& valor) : data(valor), left(nullptr), right(nullptr) {}
         
-
-        // Obtener el siguiente nodo en la lista
-        Node* getNext() { return next_; }
-
-        // Obtener los datos del nodo
         const T& getData() const {
-            return data_;
+            return data;
         }
-        T& getData() {
-            return data_;
+        
+        Node* getLeft() const {
+            return left;
         }
-        Node* Raiz;
-        unsigned int size = 0;
+        
+        Node* getRight() const {
+            return right;
+        }
+        
+        void setData(const T& newdata) {
+            data = newdata;
+        }
+
+        void setLeft(Node* newleft) {
+            left = newleft;
+        }
+
+        void setRight(Node* newright) {
+            right = newright;
+        }
     };
-    
-    public:
-    BST (): izq(nullptr), der(nullptr), data(0);
 
-    BST (T elem): data = elem, izq = nullptr, der = nullptr;
+    Node* Raiz;
+    unsigned int size;
 
-    bool empty(){
-        BST* n;
-        return n = nullptr;
-    }
-
-    void Insert (T elem){
-        if(empty()){
-            *BST n = New BST(elem);
-        }
-        else{
-            if(elem > n->data){
-                n->der = Insert(n->der,elem);
+    void Insert(Node* n, const T& newdata) {
+        if (n->getData() > newdata) {
+            if (n->getLeft() != nullptr) {
+                Insert(n->getLeft(), newdata);
             }
-            else if{
-                n->izq = Insert(n->izq,elem);
+            else {
+                n->setLeft(new Node(newdata));
             }
         }
-        return n;
-    }
-    void Ordenar(BST* n){
-        if (empty()){
-            return 0;
+        else if (n->getData() < newdata) {
+            if (n->getRight() != nullptr) {
+                Insert(n->getRight(), newdata);
+            }
+            else {
+                n->setRight(new Node(newdata));
+            }
         }
-        Ordenar(n->izq);
-        cout << n->data << " ";
-        Ordenar(n->der);
+        else {
+            return;
+        }
     }
-    
+
+    void deleteNode(Node* aux, Node* n) {
+        if (n->getLeft() == nullptr && n->getRight() == nullptr) {
+            if (aux->getLeft() == n) {
+                aux->setLeft(nullptr);
+            } else {
+                aux->setRight(nullptr);
+            }
+            delete n;
+        } else if (n->getLeft() != nullptr && n->getRight() == nullptr) {
+            if (aux->getLeft() == n) {
+                aux->setLeft(n->getLeft());
+            } else {
+                aux->setRight(n->getLeft());
+            }
+            delete n;
+        } else if (n->getLeft() == nullptr && n->getRight() != nullptr) {
+            if (aux->getLeft() == n) {
+                aux->setLeft(n->getRight());
+            } else {
+                aux->setRight(n->getRight());
+            }
+            delete n;
+        } else {
+            Node* temp = n->getRight();
+            while (temp->getLeft() != nullptr) {
+                temp = temp->getLeft();
+            }
+            T value = temp->getData();
+            deleteRecursive(Raiz, temp->getData());
+            n->setData(value);
+        }
+    }
+
+    void deleteRecursive(Node* n, const T& val) {
+        if (n == nullptr) {
+            return;
+        }
+        if (val < n->getData()) {
+            if (n->getLeft() != nullptr) {
+                if (n->getLeft()->getData() == val) {
+                    deleteNode(n, n->getLeft());
+                } else {
+                    deleteRecursive(n->getLeft(), val);
+                }
+            }
+        } else {
+            if (n->getRight() != nullptr) {
+                if (n->getRight()->getData() == val) {
+                    deleteNode(n, n->getRight());
+                } else {
+                    deleteRecursive(n->getRight(), val);
+                }
+            }
+        }
+    }
+
+    void Print(Node* n) const {
+        if (n != nullptr) {
+            Print(n->getLeft());
+            cout << n->getData() << endl;
+            Print(n->getRight());
+        }
+    }
+
+    bool find(Node* n, const T& valor) const {
+        if (n == nullptr) {
+            return false;
+        }
+        if (n->getData() == valor) {
+            return true;
+        }
+        if (valor < n->getData()) {
+            return find(n->getLeft(), valor);
+        }
+        return find(n->getRight(), valor);
+    }
+public:
+    binary_tree() : Raiz(nullptr), size(0) {}
+
+    bool empty() const {
+        return Raiz == nullptr;
+    }
+
+    void Insert(const T& newdata) {
+        if (Raiz == nullptr) {
+            Raiz = new Node(newdata);
+        }
+        else {
+            Insert(Raiz, newdata);
+        }
+        size++;
+    }
+
+    void mostrar() const {
+        Print(Raiz);
+    }
+
+    bool find(const T& data) const {
+        return find(Raiz, data);
+    }
+
+    void remove(const T& val) {
+        if (find(val)) {
+            deleteRecursive(Raiz, val);
+            size--;
+        }
+    }
 };
 
-int main(){
-    BST<int> a;
-    a.Insert(32);
-    a.Insert(12);
-    a.Ordenar(a);
-}
-
-
+#endif
