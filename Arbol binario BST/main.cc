2b5066297d0c82afa2dbb84ad6591835f64a5e7a
Berny02 <143149231+Berny02@users.noreply.github.com>
2024-03-22 15:20:22 -0500

    Create main.cc

---
+++ Arbol binario BST/main.cc
@@ -0,0 +1,13 @@
+#include "Arbol.hh"
+#include <iostream>
+using namespace std;
+
+int main() {
+    binary_tree<int> a;
+    a.Insert(10);
+    a.Insert(9);
+    a.Insert(19);
+    a.Insert(14);
+    a.Insert(2);
+    return 0;
+}
